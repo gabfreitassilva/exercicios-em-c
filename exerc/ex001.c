001b769863f801b979d76a2c4adcdd3f344300a6
gabfreitassilva <contatofreitas.gab@hotmail.com>
2021-08-26 16:06:51 -0300

    feito exercicio 001

---
+++ exerc/ex001.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int primeiroNumero, segundoNumero, terceiroNumero, quartoNumero;
+    char letraUm, letraDois, letraTres, letraQuatro, letraCinco, letraSeis;
+
+    primeiroNumero = 10;
+    segundoNumero = 20;
+    terceiroNumero = 30;
+    quartoNumero = 40;
+
+    letraUm = 'C';
+    letraDois = 'o';
+    letraTres = 'e';
+    letraQuatro = 'l';
+    letraCinco = 'h';
+    letraSeis = 'o';
+
+    printf("As variavéis interias são %d, %d, %d, %d\n", primeiroNumero, segundoNumero, terceiroNumero, quartoNumero);
+    printf("O animal contido nas variavéis é %c%c%c%c%c%c\n", letraUm, letraDois, letraTres, letraQuatro, letraCinco, letraSeis);
+}
