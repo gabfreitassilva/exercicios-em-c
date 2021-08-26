1c512c44f3622f36bace93386360bf4dc8088157
gabfreitassilva <contatofreitas.gab@hotmail.com>
2021-08-26 10:40:22 -0300

    ajustes na linha 8

--- aulas/ciclosAninhados.c
+++ aulas/ciclosAninhados.c
@@ -5,7 +5,7 @@ int main() {
     printf("Digite um valor: ");
     scanf("%d", &valorUser);
 
-    for (i = 0; i != valorUser; i++) {
+    for (i = 1; i <= valorUser; i++) {
         printf("Volta %d\n", i);
     }
     
