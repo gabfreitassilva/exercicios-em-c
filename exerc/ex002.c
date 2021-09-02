58506cce316b1612fe4eaf4edee126f9e633da6b
gabfreitassilva <contatofreitas.gab@hotmail.com>
2021-09-02 15:07:47 -0300

    modificado arquivo ex002.c

--- exerc/ex002.c
+++ exerc/ex002.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main() {
     int x, y, z;
+    
+    // Primeiro exemplo
     printf("Digite o primeiro número: ");
     scanf("%d", &x);
     printf("Digite o segundo número: ");
@@ -11,4 +13,18 @@ int main() {
     printf("%d\n", z = ++y);
     printf("%d\n", x = x-y+z);
     printf("%d\n", y = x - z--);
+    
+   // Segundo exemplo
+    /*
+    x = y = 10;
+    printf("x = %d \ny = %d\n", x, y);
+    z = x++;
+    printf("z = %d \nx = %d\n", z, x);
+    z = ++x;
+    printf("z = %d \nx = %d\n", z, x);
+    x = -x;
+    printf("x = %d\n", x);
+    y++;
+    printf("y = %d\n", y);
+    */
 }
